2e17d5a36960c1b4fc40ce94057fadcbb138573f
Dave Evans <devans@fastmail.com>
2022-02-21 07:19:21 -0500

    update

--- app/app.py
+++ app/app.py
@@ -1,35 +1,84 @@
+import os
 import json
 import time
 import boto3
-from boto3.dynamodb.conditions import Key
+
+TABLE_NAME = os.environ["TableName"]
+# Get the environment, so we can run appropriate scripts
+IS_DEVELOPMENT = os.environ["STAGE"] == "DEVELOPMENT"
 
 
-def get_message(lang, dynamodb=None):
+def get_dynamo_db_client():
+    """Set the dynamodb instance based on environment"""
+    if IS_DEVELOPMENT:
+        # Set the boto3 session for dynamodb if its development environment
+        dynamo_db_session = boto3.Session(profile_name="default")
+        return dynamo_db_session.resource("dynamodb")
+    else:
+        return boto3.resource("dynamodb")
 
-    client = boto3.client("dynamodb")
 
-    response = client.get_item(TableName="Translations", Key=Key("lang").eq(lang))
+DYNAMO_DB = get_dynamo_db_client()
+
+
+def get_message(lang):
+    """
+    This function will check the entry in dynamodb if exists will return item
+    :param lang
+    :return: message or return NoRecordError Exception
+    """
+
+    table = DYNAMO_DB.Table(TABLE_NAME)
+
+    response = table.get_item(
+        Key={"lang": {"S": lang}},
+        ProjectionExpression="lang, message",
+        ConsistentRead=True,
+    )
+
     # time.sleep(1)
-    return response["Items"]
+
+    if "Item" not in response:
+        raise NoRecordError("No Record Found.")
+    else:
+        return response["Item"]
 
 
-def handler(event, context):
+def lambda_handler(event, context):
     if event["queryStringParameters"] == None:
         lang = "en"
     else:
         lang = event["queryStringParameters"]["lang"]
 
     try:
+        if event["queryStringParameters"] == None:
+            lang = "en"
+        else:
+            lang = event["queryStringParameters"]["lang"]
+
+        result = get_message(lang)
+
         return {
             "statusCode": 200,
             "headers": {
                 "Content-Type": "application/json",
             },
-            "body": json.dumps(get_message(lang)),
+            "body": json.dumps(result, default=lambda o: o.__dict__),
         }
     except Exception as e:
-        print(e)
-        return {"statusCode": 500}
+        return {"statusCode": 500, "error": str(e)}
+
+
+class Error(Exception):
+    """Base class for other exceptions"""
+
+    pass
+
+
+class NoRecordError(Error):
+    """Raised when no record found"""
+
+    pass
 
 
 if __name__ == "__main__":
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,91 +1,108 @@
 from contextlib import nullcontext
 from app.app import handler, get_message
 
+DYNAMODBRESPONSE = {
+    "Item": {"lang": {"S": "en"}, "message": {"S": "Hello World"}},
+    "ResponseMetadata": {
+        "RequestId": "c4c5229b-ae73-46cc-89b1-c5f46874717b",
+        "HTTPStatusCode": 200,
+        "HTTPHeaders": {
+            "date": "Sat, 19 Feb 2022 16:40:52 GMT",
+            "content-type": "application/x-amz-json-1.0",
+            "x-amz-crc32": "2656284382",
+            "x-amzn-requestid": "c4c5229b-ae73-46cc-89b1-c5f46874717b",
+            "content-length": "58",
+            "server": "Jetty(9.4.18.v20190429)",
+        },
+        "RetryAttempts": 0,
+    },
+}
+EVENTRESPONSE = {
+    "resource": "/",
+    "path": "/",
+    "httpMethod": "GET",
+    "headers": {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US,en;q=0.9",
+        "cookie": "s_fid=7AAB6XMPLAFD9BBF-0643XMPL09956DE2; regStatus=pre-register",
+        "Host": "70ixmpl4fl.execute-api.us-east-2.amazonaws.com",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "upgrade-insecure-requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+        "X-Amzn-Trace-Id": "Root=1-5e66d96f-7491f09xmpl79d18acf3d050",
+        "X-Forwarded-For": "52.255.255.12",
+        "X-Forwarded-Port": "443",
+        "X-Forwarded-Proto": "https",
+    },
+    "multiValueHeaders": {
+        "accept": [
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+        ],
+        "accept-encoding": ["gzip, deflate, br"],
+        "accept-language": ["en-US,en;q=0.9"],
+        "cookie": ["s_fid=7AABXMPL1AFD9BBF-0643XMPL09956DE2; regStatus=pre-register;"],
+        "Host": ["70ixmpl4fl.execute-api.ca-central-1.amazonaws.com"],
+        "sec-fetch-dest": ["document"],
+        "sec-fetch-mode": ["navigate"],
+        "sec-fetch-site": ["none"],
+        "upgrade-insecure-requests": ["1"],
+        "User-Agent": [
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+        ],
+        "X-Amzn-Trace-Id": ["Root=1-5e66d96f-7491f09xmpl79d18acf3d050"],
+        "X-Forwarded-For": ["52.255.255.12"],
+        "X-Forwarded-Port": ["443"],
+        "X-Forwarded-Proto": ["https"],
+    },
+    "queryStringParameters": None,
+    "multiValueQueryStringParameters": None,
+    "pathParameters": None,
+    "stageVariables": None,
+    "requestContext": {
+        "resourceId": "2gxmpl",
+        "resourcePath": "/",
+        "httpMethod": "GET",
+        "extendedRequestId": "JJbxmplHYosFVYQ=",
+        "requestTime": "10/Mar/2020:00:03:59 +0000",
+        "path": "/Prod/",
+        "accountId": "123456789012",
+        "protocol": "HTTP/1.1",
+        "stage": "Prod",
+        "domainPrefix": "70ixmpl4fl",
+        "requestTimeEpoch": 1583798639428,
+        "requestId": "77375676-xmpl-4b79-853a-f982474efe18",
+        "identity": {
+            "cognitoIdentityPoolId": None,
+            "accountId": None,
+            "cognitoIdentityId": None,
+            "caller": None,
+            "sourceIp": "52.255.255.12",
+            "principalOrgId": None,
+            "accessKey": None,
+            "cognitoAuthenticationType": None,
+            "cognitoAuthenticationProvider": None,
+            "userArn": None,
+            "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+            "user": None,
+        },
+        "domainName": "70ixmpl4fl.execute-api.us-east-2.amazonaws.com",
+        "apiId": "70ixmpl4fl",
+    },
+    "body": None,
+    "isBase64Encoded": False,
+}
 
-def test_get_message():
+
+def test_get_message(mocker):
+    mocker.patch("app.app.boto3.client.get_item", return_value=DYNAMODBRESPONSE)
     assert get_message("en") == [{"lang": "en", "message": "Hello World"}]
 
 
 def test_handler():
-    event = {
-        "resource": "/",
-        "path": "/",
-        "httpMethod": "GET",
-        "headers": {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "accept-encoding": "gzip, deflate, br",
-            "accept-language": "en-US,en;q=0.9",
-            "cookie": "s_fid=7AAB6XMPLAFD9BBF-0643XMPL09956DE2; regStatus=pre-register",
-            "Host": "70ixmpl4fl.execute-api.us-east-2.amazonaws.com",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "none",
-            "upgrade-insecure-requests": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
-            "X-Amzn-Trace-Id": "Root=1-5e66d96f-7491f09xmpl79d18acf3d050",
-            "X-Forwarded-For": "52.255.255.12",
-            "X-Forwarded-Port": "443",
-            "X-Forwarded-Proto": "https",
-        },
-        "multiValueHeaders": {
-            "accept": [
-                "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-            ],
-            "accept-encoding": ["gzip, deflate, br"],
-            "accept-language": ["en-US,en;q=0.9"],
-            "cookie": [
-                "s_fid=7AABXMPL1AFD9BBF-0643XMPL09956DE2; regStatus=pre-register;"
-            ],
-            "Host": ["70ixmpl4fl.execute-api.ca-central-1.amazonaws.com"],
-            "sec-fetch-dest": ["document"],
-            "sec-fetch-mode": ["navigate"],
-            "sec-fetch-site": ["none"],
-            "upgrade-insecure-requests": ["1"],
-            "User-Agent": [
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
-            ],
-            "X-Amzn-Trace-Id": ["Root=1-5e66d96f-7491f09xmpl79d18acf3d050"],
-            "X-Forwarded-For": ["52.255.255.12"],
-            "X-Forwarded-Port": ["443"],
-            "X-Forwarded-Proto": ["https"],
-        },
-        "queryStringParameters": None,
-        "multiValueQueryStringParameters": None,
-        "pathParameters": None,
-        "stageVariables": None,
-        "requestContext": {
-            "resourceId": "2gxmpl",
-            "resourcePath": "/",
-            "httpMethod": "GET",
-            "extendedRequestId": "JJbxmplHYosFVYQ=",
-            "requestTime": "10/Mar/2020:00:03:59 +0000",
-            "path": "/Prod/",
-            "accountId": "123456789012",
-            "protocol": "HTTP/1.1",
-            "stage": "Prod",
-            "domainPrefix": "70ixmpl4fl",
-            "requestTimeEpoch": 1583798639428,
-            "requestId": "77375676-xmpl-4b79-853a-f982474efe18",
-            "identity": {
-                "cognitoIdentityPoolId": None,
-                "accountId": None,
-                "cognitoIdentityId": None,
-                "caller": None,
-                "sourceIp": "52.255.255.12",
-                "principalOrgId": None,
-                "accessKey": None,
-                "cognitoAuthenticationType": None,
-                "cognitoAuthenticationProvider": None,
-                "userArn": None,
-                "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
-                "user": None,
-            },
-            "domainName": "70ixmpl4fl.execute-api.us-east-2.amazonaws.com",
-            "apiId": "70ixmpl4fl",
-        },
-        "body": None,
-        "isBase64Encoded": False,
-    }
+    event = EVENTRESPONSE
 
     output = handler(event, nullcontext)
 
