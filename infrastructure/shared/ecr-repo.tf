3cb2014efb64c74e5bb39cf6f882a63c0a2c8bdd
Dave Evans <devans@duqlight.com>
2022-03-25 13:40:44 -0400

    Update ECR Repo

--- infrastructure/shared/ecr-repo.tf
+++ infrastructure/shared/ecr-repo.tf
@@ -1,9 +1,13 @@
 resource "aws_ecr_repository" "repo" {
   name                 = "pitt-cicd-demo"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
   }
 }
 
